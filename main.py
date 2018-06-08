1d007d41ec9a2ce65f39b51d4c4b701c6670a2fc
TsungYu HSIEH <i1007673219@gmail.com>
2018-06-08 18:19:31 +0800

    Pack tradition version to function

--- main.py
+++ main.py
@@ -4,45 +4,46 @@ import sys
 from threading import Thread, Lock
 import numpy as np
 
-thread_lock = Lock()
-threads = list()
+def tradition(mat1, mat2):
+    res = np.zeros((mat1.shape[0], mat2.shape[1]))
+
+    threads = list()
+    thread_lock = Lock()
+
+    def calculate(r, c):
+        tmp = float(0)
+
+        for i in range(mat1.shape[1]):
+            tmp += mat1[r, i] * mat2[i, c]
+
+        thread_lock.acquire()
+        res[r, c] = tmp
+        thread_lock.release()
+
+    for r in range(mat1.shape[0]):
+        for c in range(mat2.shape[1]):
+            thread = Thread(target=calculate, args=(r, c))
+            thread.start()
+            threads.append(thread)
+
+    for t in threads:
+        t.join()
+
+    return res
 
 def main():
     for line in sys.stdin:
-        row1, col1 = [int(s) for s in line.split()]
-        mat1 = np.zeros((row1, col1))
+        mat1 = np.zeros([int(s) for s in line.split()])
 
-        for r in range(row1):
-            mat1[r, :] = np.array([float(s) for s in input().split()])
+        for r in range(mat1.shape[0]):
+            mat1[r] = np.array([float(s) for s in input().split()])
 
-        row2, col2 = [int(s) for s in input().split()]
-        mat2 = np.zeros((row2, col2))
+        mat2 = np.zeros([int(s) for s in input().split()])
 
-        for r in range(row2):
-            mat2[r, :] = np.array([float(s) for s in input().split()])
+        for r in range(mat2.shape[0]):
+            mat2[r] = np.array([float(s) for s in input().split()])
 
-        res = np.zeros((row1, col2))
-
-        def calculate(r, c):
-            tmp = float(0)
-
-            for i in range(col1):
-                tmp += mat1[r, i] * mat2[i, c]
-
-            thread_lock.acquire()
-            res[r, c] = tmp
-            thread_lock.release()
-
-        for r in range(row1):
-            for c in range(col2):
-                thread = Thread(target=calculate, args=(r, c))
-                thread.start()
-                threads.append(thread)
-
-        for t in threads:
-            t.join()
-
-        threads.clear()
+        res = tradition(mat1, mat2)
 
         res = res.tolist()
 

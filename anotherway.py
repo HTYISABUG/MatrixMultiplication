a3eb597a62186e6147853c17386158b950c26bad
Ni-ChiTing <dfdf22222@gmail.com>
2018-06-11 05:10:50 +0800

    Another way
    Use  DyslexiaS's readfile and transport the matrix in order to calculate fast and write answer to file

--- anotherway.py
+++ anotherway.py
@@ -1,47 +1,112 @@
 #!/usr/bin/python3
+import sys
+from threading import Thread, Lock
+import numpy as np
+
 def readFile():
-    print('ANOTHER MATRIX MULTUPLICATION')
-    filename = input('Filename: ')
-    with open(filename) as f:
-        matrix_A = []
-        matrix_B = []
-        i = 1
-        for line in f:
-            if i == 1:
-                A_row, A_col = [int(x) for x in line.split()] 
-                matrix = matrix_A
-            elif i == A_row+2:
-                B_row, B_col = [int(x) for x in line.split()] 
-                matrix = matrix_B
-            else:
-                matrix.append([int(x) for x in line.split()])
-            i = i + 1;
-        f.close()
-        return matrix_A, matrix_B
-    
+    for line in sys.stdin:
+        mat1 = np.zeros([int(s) for s in line.split()])
+
+        for r in range(mat1.shape[0]):
+            mat1[r] = np.array([float(s) for s in input().split()])
+
+        mat2 = np.zeros([int(s) for s in input().split()])
+
+        for r in range(mat2.shape[0]):
+            mat2[r] = np.array([float(s) for s in input().split()])
+    return (mat1.tolist(), mat2.tolist())
+
 def mul(A, B):
     length = len(A)
-    length2 = len(A[0])
-    length3 = len(B[0])
-    if len(A[0])!=len(B):
-        print("Size error")
-        return
-    C = [[0 for j in range(0, length)] for i in range(0, length3)] #length*length2
-    for k in range(0, length2):
-        for i in range(0, length):
-            for j in range(0, length3):
-                C[i][j] += A[i][k] * B[k][j] #cache hit max 
-    return C
+    result = [[0 for j in range(0, length)] for i in range(0, length)] #length*length3
+    #for k in range(0, length2):
+    #    for i in range(0, length):
+    #        for j in range(0, length3):
+    #            result[i][j] += A[i][k] * B[k][j] #cache hit max
+
+    for i in range(0, length):
+        for j in range(0, length):
+            for k in range(0, length):
+                result[i][j] += A[i][k] * B[j][k]
+    return result
 
 def print_matrix(result_m):
+    answer = open('answer.txt', 'w')
     for line in result_m:
-        print(" ".join(map(str,line)))
+        answer.write(" ".join(map(str,line)))
+        answer.write("\n")
+    answer.close()
+
+def add(A, B):
+    n = len(A)
+    C = []
+    for i in range(0, n):
+        C.append([x+y for x,y in zip(A[i], B[i])])
+    return C
+
+def ToSquare(matrix):
+    if(len(matrix)!=len(matrix[0])):
+        max_value=max(len(matrix),len(matrix[0]))
+        new_matrix=[[0 for j in range(0, max_value)] for i in range(0, max_value)]
+        for i in range(0, len(matrix)):
+            for j in range(0, len(matrix[0])):
+                new_matrix[i][j]=matrix[i][j]
+        return new_matrix
+    else:
+        return matrix
+    
+def ToOriginalSize(result,matrixA,matrixB):
+    length = len(matrixA)
+    length2 = len(matrixB[0])
+    new_matrix=[[0 for j in range(0, length)] for i in range(0, length2)]
+    for i in range(0, length):
+            for j in range(0, (length2)):
+                new_matrix[i][j]=result[i][j]
+    return new_matrix
 
 def div_matrix(matrix1,matrix2):
-    print(len(matrix1))
-    print(len(matrix2))
-
+    n = len(matrix1)
+    matrix2= (list(map(list, zip(*matrix2))))
+   # print(matrix2)
+    if n <= 2:
+        return mul(matrix1,matrix2)
+    else:
+        new_size = int(n/2)
+        a11 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        a12 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        a21 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        a22 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        
+        b11 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        b12 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        b21 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        b22 = [[0 for j in range(0, new_size)] for i in range(0, new_size)]
+        for i in range(0, new_size):
+            for j in range(0, new_size):
+                a11[i][j] = matrix1[i][j]
+                a12[i][j] = matrix1[i][j+new_size]
+                a21[i][j] = matrix1[i+new_size][j]
+                a22[i][j] = matrix1[i+new_size][j+new_size]
+            
+                b11[i][j] = matrix2[i][j]
+                b12[i][j] = matrix2[i][j+new_size]
+                b21[i][j] = matrix2[i+new_size][j]
+                b22[i][j] = matrix2[i+new_size][j+new_size]
+        c11=add(mul(a11,b11),mul(a12,b12))
+        c12=add(mul(a11,b21),mul(a12,b22))
+        c21=add(mul(a21,b11),mul(a22,b12))
+        c22=add(mul(a21,b21),mul(a22,b22))
+        C = [[0 for j in range(0, n)] for i in range(0, n)]
+        for i in range(0, new_size):
+            for j in range(0, new_size):
+                C[i][j] = c11[i][j]
+                C[i][j+new_size] = c12[i][j]
+                C[i+new_size][j] = c21[i][j]
+                C[i+new_size][j+new_size] = c22[i][j]
+        return C
+            
 if __name__ == '__main__':
         matrix_A, matrix_B = readFile()
-        print_matrix(mul(matrix_A,matrix_B))
-        div_matrix(matrix_A,matrix_B)
+        #matrixB= (list(map(list, zip(*matrix_B))))
+        #print_matrix(ToOriginalSize(mul(ToSquare(matrix_A),ToSquare(matrixB)),matrix_A, matrix_B ))
+        print_matrix(ToOriginalSize(div_matrix(ToSquare(matrix_A),ToSquare(matrix_B)),matrix_A, matrix_B ))
